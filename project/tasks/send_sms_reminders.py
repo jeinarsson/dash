49004f687348843b9be45f170e26b85b4b555694
Jonas Einarsson <me@jonaseinarsson.se>
2018-06-05 11:42:23 -0700

    remove hard coded twilio phone number...

--- project/tasks/send_sms_reminders.py
+++ project/tasks/send_sms_reminders.py
@@ -1,25 +1,12 @@
-# Download the helper library from https://www.twilio.com/docs/python/install
 from twilio.rest import Client
 
-##
-## Load config
-##
-
-try:
-    import project.dash_config as dc
-except ModuleNotFoundError as e:
-    import project.dash_config_default as dc
-
-
-# Your Account Sid and Auth Token from twilio.com/console
+import project.dash_config as dc
 account_sid = dc.TWILIO['account-sid']
 auth_token = dc.TWILIO['auth-token']
+phone_number = dc.TWILIO['phone-number']
 
-client = Client(account_sid, auth_token)
-message = client.messages.create(
-                              body='test reminder..',
-                              from_='+16502032813',
-                              to='+16507042055'
-                          )
+def send(to, body):
+	client = Client(account_sid, auth_token)
+	message = client.messages.create(body=body, from_=phone_number, to=to)
 
-print(message.sid)
+
